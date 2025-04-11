b62b997503eeb12a07761f4b9936cd208c06f8ec
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2025-04-11 15:56:03 +0100

    feat: Tokenizer.

--- team-red/src/tokenizer.py
+++ team-red/src/tokenizer.py
@@ -7,14 +7,16 @@ from transformers import AutoTokenizer
 
 
 class Tokenizer:
-    
-    def __init__(self, model, token= None):
+
+    def __init__(self, model, token=None):
         kwargs = {'use_auth_token': token} if token else {}
         self.tokenizer = AutoTokenizer.from_pretrained(model, **kwargs)
 
-    def tokenize(self, text):
-        return self.tokenizer(text, return_tensors='pt', truncation=True)
+    def tokenize(self, text, max_length=None):
+        kwargs = {'max_length': max_length} if max_length else {}
+        defaults = {'truncation': True, 'return_tensors': 'pt'}
+
+        return self.tokenizer(text, **{**defaults, **kwargs})
 
     def decode(self, outputs):
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-    

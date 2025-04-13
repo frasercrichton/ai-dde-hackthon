318c59a0782b663122a498b7e4652ce0262fa90e
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2025-04-13 11:15:36 +0100

    feat: Tokenizer eos as property.

--- team-red/src/tokenizer.py
+++ team-red/src/tokenizer.py
@@ -13,9 +13,11 @@ class Tokenizer:
         self.tokenizer = AutoTokenizer.from_pretrained(model, **kwargs)
         print(self.tokenizer.eos_token)  # should print <|endoftext|>
 
+    @property
     def eos_token(self):
         return self.tokenizer.eos_token
-
+    
+    @property
     def eos_token_id(self):
         return self.tokenizer.eos_token_id
 
